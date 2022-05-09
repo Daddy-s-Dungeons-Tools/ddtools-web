802231a4e2a4be39d133800d7ac79f8d41f60995
Frank Matranga <thefrankmatranga@gmail.com>
2022-05-09 12:36:37 -0400

    Add ChakraUI to frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
+
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
