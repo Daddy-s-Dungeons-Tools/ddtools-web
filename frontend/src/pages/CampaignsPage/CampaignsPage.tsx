5dea13f8432041f220dd981bdf8c156fbf8c5a15
Frank Matranga <thefrankmatranga@gmail.com>
2022-05-11 14:00:56 -0400

    List DM campaigns

--- frontend/src/pages/CampaignsPage/CampaignsPage.tsx
+++ frontend/src/pages/CampaignsPage/CampaignsPage.tsx
@@ -7,10 +7,16 @@ import {
   Heading,
   VStack,
   Button,
+  Skeleton,
+  Stack,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { NewCampaignModal } from "../../components/NewCampaignModal/NewCampaignModal";
 import { useProtectedRoute } from "../../hooks/routes";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { collection, query, where } from "firebase/firestore";
+import { auth, firestore } from "../../services/firebase";
+import { Campaign } from "ddtools-types";
 
 function PlayerCampaignBox() {
   return (
@@ -41,9 +47,48 @@ function PlayerCampaignBox() {
   );
 }
 
+type DMCampaignBoxPropTypes = {
+  campaign: Campaign;
+};
+function DMCampaignBox({ campaign }: DMCampaignBoxPropTypes) {
+  return (
+    <Box minW="100%" borderWidth="1px" borderRadius="lg" overflow="hidden">
+      <Flex>
+        <Image
+          w={"32"}
+          src="https://cdn.vox-cdn.com/thumbor/ShgZ3-pi6BnxczAG1ycmmk3l8uE=/0x23:1513x1032/1400x1400/filters:focal(0x23:1513x1032):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/33511669/905825_10152396043776071_8883312392855826763_o.0.jpg"
+          alt=""
+        />
+        <Box p={6}>
+          <Heading size="md" mb="8">
+            {campaign.name}
+          </Heading>
+
+          <Box
+            color="gray.500"
+            fontWeight="semibold"
+            fontSize="xs"
+            textTransform="uppercase"
+          >
+            {campaign.playerUserIds?.length ?? 0} players | ?? sessions | DMed
+            by ??
+          </Box>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
+
 export default function CampaignsPage() {
   const [isNewCampaignModalOpen, setIsNewCampaignModalOpen] =
     useState<boolean>(false);
+  const [dmCampaigns, isDMCampaignsLoading, dmCampaignsError] =
+    useCollectionData(
+      query(
+        collection(firestore, "campaigns"),
+        where("dmUserIds", "array-contains", auth.currentUser?.uid)
+      )
+    );
 
   useProtectedRoute();
 
@@ -69,6 +114,20 @@ export default function CampaignsPage() {
         {/* DM listing */}
         <VStack flex={"1"} align="flex-start" p={3}>
           <Heading mb="8">Your DM Campaigns</Heading>
+
+          {isDMCampaignsLoading ? (
+            <Stack w="100%">
+              <Skeleton height="130px" />
+            </Stack>
+          ) : (
+            dmCampaigns?.map((campaign) => (
+              <DMCampaignBox
+                key={campaign.id}
+                campaign={campaign as Campaign}
+              />
+            ))
+          )}
+
           <Button minW="100%" onClick={() => setIsNewCampaignModalOpen(true)}>
             Start Campaign
           </Button>
