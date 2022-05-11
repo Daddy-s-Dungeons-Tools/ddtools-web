773882430249b3b849a97ea21c3752b1a313f2f3
Frank Matranga <thefrankmatranga@gmail.com>
2022-05-11 13:02:36 -0400

    Add loading spinner

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,24 @@
+import { Center, Spinner } from "@chakra-ui/react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Outlet } from "react-router-dom";
 import TopNavbar from "./components/TopNavbar/TopNavbar";
+import { auth } from "./services/firebase";
 
 function App() {
+  const [user, isUserLoading, userError] = useAuthState(auth);
+
   return (
     <div className="App">
       <header>
         <TopNavbar />
       </header>
-      <Outlet />
+      {isUserLoading ? (
+        <Center pos={"fixed"} top={0} left={0} w={"100%"} h="100vh">
+          <Spinner size={"xl"} />
+        </Center>
+      ) : (
+        <Outlet />
+      )}
     </div>
   );
 }
