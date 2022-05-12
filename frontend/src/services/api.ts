0dea38c7bea878264cd2626f6a1ced350e3fcd68
Frank Matranga <thefrankmatranga@gmail.com>
2022-05-11 22:53:55 -0400

    Check for existing campaign by ID

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -6,6 +6,7 @@ import {
   updateDoc,
   arrayUnion,
   arrayRemove,
+  getDoc,
 } from "firebase/firestore";
 import { campaignConverter } from "./converter";
 import { auth, firestore } from "./firebase";
@@ -14,12 +15,22 @@ const campaignCollection = collection(firestore, "campaigns").withConverter(
   campaignConverter
 );
 
-export function addCampaign(name: string, dmInviteEmails: string[]) {
+/** Add a new campaign with the desired name and DM invitations. */
+export async function addCampaign({
+  name,
+  dmInviteEmails,
+}: {
+  name: string;
+  dmInviteEmails: string[];
+}) {
   if (!name || name.trim().length === 0) return;
 
   const docId = name.toLowerCase().replaceAll(" ", "-");
 
-  // TODO: CHECK IF DOCID ALREADY EXISTS
+  // CHECK IF DOCID ALREADY EXISTS
+  if ((await getDoc(doc(campaignCollection, docId))).exists()) {
+    throw new Error("Campaign already exists with that ID.");
+  }
 
   const dmUserIds = [];
   if (auth.currentUser?.uid) {
@@ -33,6 +44,7 @@ export function addCampaign(name: string, dmInviteEmails: string[]) {
   });
 }
 
+/** Add desired players (by their user IDs) to a campaign. */
 export function addPlayersToCampaign(
   campaignId: Campaign["id"],
   playerUserIds: string[]
@@ -42,6 +54,7 @@ export function addPlayersToCampaign(
   });
 }
 
+/** Remove the desired players (by their user IDs) from a campaign. */
 export function removePlayerCampaignInvites(
   campaignId: Campaign["id"],
   playerUserEmails: string[]
