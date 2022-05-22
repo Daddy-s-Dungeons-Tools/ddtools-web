613d9f49230ef9927272b3ba65176af8b2752ed9
Frank Matranga <thefrankmatranga@gmail.com>
2022-05-22 10:04:13 -0400

    Add prettier and format all

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,10 @@
-import {Campaign} from "ddtools-types";
+import { Campaign } from "ddtools-types";
 import * as functions from "firebase-functions";
-import {initializeApp} from "firebase-admin/app";
-import {getAuth} from "firebase-admin/auth";
+import { initializeApp } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
 
 /** Firebase admin app with full permissions */
 const app = initializeApp();
-
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
@@ -16,32 +15,35 @@ const app = initializeApp();
 // });
 
 export const modifyCampaign = functions.firestore
-    .document("campaigns/{campaignId}")
-    .onWrite(async (change, context) => {
+  .document("campaigns/{campaignId}")
+  .onWrite(async (change, context) => {
     // Get an object with the current document value.
     // If the document does not exist, it has been deleted.
-      const campaign: Campaign | null = change.after.exists ?
-        change.after.data() as Campaign : null;
+    const campaign: Campaign | null = change.after.exists
+      ? (change.after.data() as Campaign)
+      : null;
 
-      // Get an object with the previous document value (for update or delete)
-      // const oldCampaign: Campaign | null =
-      //   change.before.exists ? change.before.data() as Campaign : null;
+    // Get an object with the previous document value (for update or delete)
+    // const oldCampaign: Campaign | null =
+    //   change.before.exists ? change.before.data() as Campaign : null;
 
-      if (!campaign) {
-        return;
-      }
+    if (!campaign) {
+      return;
+    }
 
-      // Calculate DM user names
-      let dmUserNames: string[] = [];
-      if (campaign.dmUserIds) {
-        const dmUsers = await getAuth(app)
-            .getUsers(campaign.dmUserIds?.map((uid) => ({uid})));
+    // Calculate DM user names
+    let dmUserNames: string[] = [];
+    if (campaign.dmUserIds) {
+      const dmUsers = await getAuth(app).getUsers(
+        campaign.dmUserIds?.map((uid) => ({ uid })),
+      );
 
-        dmUserNames = dmUsers.users.map((user) =>
-          user.displayName?.split(" ")[0] || user.email!);
-      }
+      dmUserNames = dmUsers.users.map(
+        (user) => user.displayName?.split(" ")[0] || user.email!,
+      );
+    }
 
-      return change.after.ref.update({
-        dmUserNames,
-      });
+    return change.after.ref.update({
+      dmUserNames,
     });
+  });
