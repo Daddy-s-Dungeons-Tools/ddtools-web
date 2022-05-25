1776b1057d03f7fc1f634db3867355cd9bed3c4a
Frank Matranga <thefrankmatranga@gmail.com>
2022-05-24 21:52:13 -0400

    Add TopNavbar component

---
+++ frontend/src/stories/TopNavbar.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import TopNavbar from "../components/TopNavbar/TopNavbar";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "DDTools/TopNavbar",
+  component: TopNavbar,
+} as ComponentMeta<typeof TopNavbar>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof TopNavbar> = (args) => <TopNavbar />;
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {};
