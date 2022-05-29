5f0b196aec752281a3e1067166b544d2b2f451e3
Frank Matranga <thefrankmatranga@gmail.com>
2022-05-28 22:32:37 -0400

    Display current avatar in modal

--- frontend/src/components/UserAvatarModal/UserAvatarModal.tsx
+++ frontend/src/components/UserAvatarModal/UserAvatarModal.tsx
@@ -1,5 +1,7 @@
 import {
+  Box,
   Button,
+  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -7,29 +9,49 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  VStack,
 } from "@chakra-ui/react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../services/firebase";
+import { UserAvatar } from "../UserAvatar";
+
+type UserAvatarModalPropTypes = {
+  isOpen: boolean;
+  onClose: () => void;
+};
 
 /**
  * Modal that allows the user to view their current avatar image (if set)
  * and upload a new one. Does not currently support cropping or panning the image.
  */
-export function UserAvatarModal() {
-  const [user, isUserLoading, userError] = useAuthState(auth);
+export function UserAvatarModal({ isOpen, onClose }: UserAvatarModalPropTypes) {
+  const [user, isUserLoading] = useAuthState(auth);
 
   return (
-    <Modal isOpen={true} onClose={() => {}}>
+    <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Update Avatar</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>
-          {/* TODO: Display image */}
-          {/* TODO: File input for new image */}
-        </ModalBody>
+        {!isUserLoading && user && (
+          <ModalBody>
+            <VStack spacing="10">
+              <UserAvatar
+                size="2xl"
+                userId={user.uid}
+                userDisplayName={user.displayName}
+              />
+
+              <Box>
+                <Input type="file" />
+              </Box>
+            </VStack>
+          </ModalBody>
+        )}
         <ModalFooter>
-          <Button colorScheme="purple">Save</Button>
+          <Button colorScheme="purple" disabled={isUserLoading}>
+            Upload
+          </Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
