a3ec0c7025380f611bc34bd9e7546bb99d73f59b
Frank Matranga <thefrankmatranga@gmail.com>
2022-05-29 18:11:50 -0400

    Fetch and list actual campaign characters

--- frontend/src/pages/CampaignPages/dashboards/components/Party.tsx
+++ frontend/src/pages/CampaignPages/dashboards/components/Party.tsx
@@ -11,15 +11,26 @@ import {
   useToast,
   Badge,
   Divider,
+  Skeleton,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  Alert,
 } from "@chakra-ui/react";
+import { Character } from "ddtools-types";
+import { collection, orderBy, query } from "firebase/firestore";
 import { useContext } from "react";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 import { CampaignPlayerBox } from "../../../../components/CampaignPlayerBox/CampaignPlayerBox";
 import {
   addUserCampaignInvites,
   removeUserCampaignInvites,
 } from "../../../../services/api";
-import { testCharacter } from "../../../../utils/consts";
+import { converterFactory } from "../../../../services/converter";
+import { firestore } from "../../../../services/firebase";
 import { CampaignUserContext } from "../../CampaignDashboardPage";
+
+const characterConverter = converterFactory<Character>();
 
 function CampaignUsersManager() {
   const toast = useToast();
@@ -105,17 +116,57 @@ function CampaignUsersManager() {
 
 export default function Party() {
   const { userRole, campaign } = useContext(CampaignUserContext);
+
+  const [
+    campaignCharacters,
+    isCampaignCharactersLoading,
+    campaignCharactersError,
+  ] = useCollectionData(
+    query(
+      collection(firestore, "campaigns", campaign.id, "characters"),
+      orderBy("name"),
+    ).withConverter(characterConverter),
+  );
+
   return (
     <Box>
       <VStack align="flex-start" spacing="8">
         <VStack minW="100%">
-          {campaign.playerUserIds?.map((userId) => (
-            <CampaignPlayerBox
-              key={userId}
-              userDisplayName="User"
-              character={testCharacter}
-            />
-          ))}
+          {campaignCharactersError && (
+            <Alert status="error">
+              <AlertIcon />
+              <AlertTitle>Yikes!</AlertTitle>
+              <AlertDescription>
+                There was an error while fetching this campaign's characters...
+              </AlertDescription>
+            </Alert>
+          )}
+          {!isCampaignCharactersLoading &&
+            campaignCharacters &&
+            campaignCharacters.map((character) => (
+              <CampaignPlayerBox
+                key={character.id}
+                userDisplayName={
+                  (campaign.userSummaries &&
+                    character.id in campaign.userSummaries &&
+                    campaign.userSummaries[character.id].displayName) ||
+                  "User"
+                }
+                character={character}
+              />
+            ))}
+
+          {!isCampaignCharactersLoading && campaignCharacters?.length === 0 && (
+            <Text>There are no adventurers in this campaign yet!</Text>
+          )}
+
+          {isCampaignCharactersLoading && (
+            <VStack>
+              {[...Array(4)].map((_, i) => (
+                <Skeleton key={i} height="150px" />
+              ))}
+            </VStack>
+          )}
         </VStack>
 
         {userRole === "dm" && (
