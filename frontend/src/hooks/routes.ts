e84f5e78326ea682fb5f97929d45c2b96ed58a4e
Frank Matranga <thefrankmatranga@gmail.com>
2022-05-28 21:01:39 -0400

    Document useProtectedRoute

--- frontend/src/hooks/routes.ts
+++ frontend/src/hooks/routes.ts
@@ -3,8 +3,12 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../services/firebase";
 
+/**
+ * Redirects to / if the user is not logged in.
+ * Sets the current URL in localStorage so it can be redirected to after login.
+ **/
 export function useProtectedRoute() {
-  const [user, isUserLoading, userError] = useAuthState(auth);
+  const [user, isUserLoading] = useAuthState(auth);
   const navigate = useNavigate();
 
   useEffect(() => {
