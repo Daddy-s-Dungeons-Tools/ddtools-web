d4f3a746bcb21b15db86c098da26a654e8648f4d
Frank Matranga <thefrankmatranga@gmail.com>
2022-06-01 01:56:10 -0400

    Add character creator container

--- frontend/src/pages/CampaignPages/dashboards/components/CharacterCreator.tsx
+++ frontend/src/pages/CampaignPages/dashboards/components/CharacterCreator.tsx
@@ -1,9 +1,19 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Button, Container, Heading, Text } from "@chakra-ui/react";
+import { useContext } from "react";
+import { CampaignUserContext } from "../../CampaignDashboardPage";
 
 export function CharacterCreator() {
+  const { campaign } = useContext(CampaignUserContext);
   return (
-    <Box>
+    <Container maxW="container.md">
       <Heading size="2xl">Character Creator</Heading>
-    </Box>
+      <Text color="gray.500" fontWeight="semibold" fontSize="xl">
+        Enter in all of your character details once to get started.
+      </Text>
+
+      <Button colorScheme="purple" minW="100%" size="lg" mt="5">
+        Enter {campaign.name} as {"CHARACTER"}
+      </Button>
+    </Container>
   );
 }
