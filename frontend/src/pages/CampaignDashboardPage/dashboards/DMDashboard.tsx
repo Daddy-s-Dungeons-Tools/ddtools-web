f09580c321d6dedc1b7b07ccea4c6c8240a3f40d
Frank Matranga <thefrankmatranga@gmail.com>
2022-06-09 02:39:45 -0400

    Mobile display

--- frontend/src/pages/CampaignDashboardPage/dashboards/DMDashboard.tsx
+++ frontend/src/pages/CampaignDashboardPage/dashboards/DMDashboard.tsx
@@ -1,4 +1,19 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Grid,
+  GridItem,
+  HStack,
+  Icon,
+  IconButton,
+  Show,
+  VStack,
+} from "@chakra-ui/react";
+import { useState } from "react";
 import { WidgetContainer } from "./components/WidgetContainer";
 import {
   audioManagerWidget,
@@ -10,40 +25,86 @@ import {
   mapWidget,
   notesWidget,
   npcCreatureWidget,
+  Widget,
 } from "./components/widgets";
 
 export function DMDashboard() {
+  // Only for sidebar on mobile
+  const [sidebarWidget, setSidebarWidget] = useState<Widget | null>(null);
   return (
-    <Grid
-      height="100%"
-      templateRows="repeat(3, 1fr)"
-      templateColumns="repeat(6, 1fr)"
-      gap="3"
-    >
-      <GridItem rowSpan={2} colSpan={{ base: 6, lg: 2 }}>
-        <WidgetContainer
-          widgets={[
-            notesWidget,
-            charactersWidget,
-            npcCreatureWidget,
-            logWidget,
-          ]}
-        />
-      </GridItem>
+    <>
+      <Show above="lg">
+        <Grid
+          height="100%"
+          templateRows="repeat(3, 1fr)"
+          templateColumns="repeat(6, 1fr)"
+          gap="3"
+        >
+          <GridItem rowSpan={2} colSpan={{ base: 6, lg: 2 }}>
+            <WidgetContainer
+              widgets={[
+                notesWidget,
+                charactersWidget,
+                npcCreatureWidget,
+                logWidget,
+              ]}
+            />
+          </GridItem>
 
-      <GridItem rowSpan={3} colSpan={{ base: 6, lg: 4 }}>
-        <WidgetContainer widgets={[mapWidget, battleMapsWidget]} />
-      </GridItem>
+          <GridItem rowSpan={3} colSpan={{ base: 6, lg: 4 }}>
+            <WidgetContainer widgets={[mapWidget, battleMapsWidget]} />
+          </GridItem>
 
-      <GridItem rowSpan={1} colSpan={{ base: 6, lg: 2 }}>
-        <WidgetContainer
-          widgets={[
-            audioManagerWidget,
-            manageUsersWidget,
-            campaignSettingsWidget,
-          ]}
-        />
-      </GridItem>
-    </Grid>
+          <GridItem rowSpan={1} colSpan={{ base: 6, lg: 2 }}>
+            <WidgetContainer
+              widgets={[
+                audioManagerWidget,
+                manageUsersWidget,
+                campaignSettingsWidget,
+              ]}
+            />
+          </GridItem>
+        </Grid>
+      </Show>
+      <Show below="lg">
+        <>
+          {sidebarWidget && (
+            <Drawer
+              isOpen
+              placement="bottom"
+              onClose={() => setSidebarWidget(null)}
+            >
+              <DrawerOverlay />
+              <DrawerContent>
+                <DrawerCloseButton />
+                <DrawerHeader>{sidebarWidget.label}</DrawerHeader>
+
+                <DrawerBody>{sidebarWidget.component}</DrawerBody>
+              </DrawerContent>
+            </Drawer>
+          )}
+          <VStack h="80vh">
+            <HStack>
+              {[
+                notesWidget,
+                charactersWidget,
+                npcCreatureWidget,
+                logWidget,
+                audioManagerWidget,
+                manageUsersWidget,
+                campaignSettingsWidget,
+              ].map((widget) => (
+                <IconButton
+                  aria-label={widget.ariaLabel}
+                  icon={<Icon as={widget.icon} />}
+                  onClick={() => setSidebarWidget(widget)}
+                />
+              ))}
+            </HStack>
+            <WidgetContainer flex="1" widgets={[mapWidget, battleMapsWidget]} />
+          </VStack>
+        </>
+      </Show>
+    </>
   );
 }
