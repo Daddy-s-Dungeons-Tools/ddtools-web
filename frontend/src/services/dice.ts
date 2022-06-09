d60f55849051d031518dd66c6bf95d7743362357
Frank Matranga <thefrankmatranga@gmail.com>
2022-06-09 02:29:10 -0400

    Switch dice themes

--- frontend/src/services/dice.ts
+++ frontend/src/services/dice.ts
@@ -2,6 +2,9 @@ import DiceBox from "@3d-dice/dice-box";
 
 export const diceBox = new DiceBox("#dice-box", {
   assetPath: "/assets/dice-box/",
+  restitution: 0.1,
+  theme: "diceOfRolling",
+  scale: 5,
 });
 
 //@ts-ignore
