f442e0552d2b0d3f29126b3fe0fff02d4a0aa056
Frank Matranga <thefrankmatranga@gmail.com>
2022-06-12 18:02:37 -0400

    Scroll, pan, and zoom battle map

--- frontend/src/pages/CampaignDashboardPage/widgets/BattleMaps.tsx
+++ frontend/src/pages/CampaignDashboardPage/widgets/BattleMaps.tsx
@@ -1,4 +1,5 @@
 import { Box } from "@chakra-ui/react";
+import { KonvaEventObject } from "konva/lib/Node";
 import { useEffect, useMemo, useRef, useState } from "react";
 import { Layer, Line, Rect, Stage } from "react-konva";
 import { debounce } from "utils/debounce";
@@ -7,27 +8,34 @@ import { debounce } from "utils/debounce";
 const GRID_CELL_SIZE = 50;
 const GRID_WIDTH = 1000;
 const GRID_HEIGHT = 1000;
+const SCALE_BY = 1.2;
 
 export function BattleMaps() {
   const boxRef = useRef<HTMLDivElement>(null);
-  const [stageDimensions, setStageDimensions] = useState<{
+  const [stage, setStage] = useState<{
+    x: number;
+    y: number;
+    scale: number;
     width: number;
     height: number;
   }>({
+    x: 0,
+    y: 0,
     width: 500,
     height: 500,
+    scale: 1,
   });
 
   /** Set the stage size to the size of the parent container. */
   function resizeStage() {
-    console.log("resizing");
     if (!boxRef.current) {
       return;
     }
 
     const { offsetWidth, offsetHeight } = boxRef.current;
 
-    setStageDimensions({
+    setStage({
+      ...stage,
       width: offsetWidth,
       height: offsetHeight,
     });
@@ -76,24 +84,62 @@ export function BattleMaps() {
       );
   }, []);
 
+  function handleScrollWheel(konvaEvent: KonvaEventObject<WheelEvent>) {
+    konvaEvent.evt.preventDefault();
+
+    const curStage = konvaEvent.target.getStage()!;
+    const oldScale = curStage.scaleX();
+    const mousePointTo = {
+      x: curStage.getPointerPosition()!.x / oldScale - curStage.x() / oldScale,
+      y: curStage.getPointerPosition()!.y / oldScale - curStage.y() / oldScale,
+    };
+
+    const newScale =
+      konvaEvent.evt.deltaY < 0 ? oldScale * SCALE_BY : oldScale / SCALE_BY;
+
+    setStage({
+      ...stage,
+      scale: newScale,
+      x:
+        (curStage.getPointerPosition()!.x / newScale - mousePointTo.x) *
+        newScale,
+      y:
+        (curStage.getPointerPosition()!.y / newScale - mousePointTo.y) *
+        newScale,
+    });
+  }
+
+  function handleStageDragEnd(konvaEvent: KonvaEventObject<DragEvent>) {
+    setStage({
+      ...stage,
+      x: konvaEvent.target.x(),
+      y: konvaEvent.target.y(),
+    });
+  }
+
   return (
     <Box ref={boxRef} minW="100%" minH="100%">
       <Stage
-        width={stageDimensions.width}
-        height={stageDimensions.height}
+        x={stage.x}
+        y={stage.y}
+        width={stage.width}
+        height={stage.height}
+        scaleX={stage.scale}
+        scaleY={stage.scale}
+        onWheel={handleScrollWheel}
         draggable
+        onDragEnd={handleStageDragEnd}
       >
         <Layer id="grid" draggable={false} x={0} y={0}>
           {gridLines}
         </Layer>
         <Layer>
           <Rect
-            x={20}
-            y={50}
-            width={100}
-            height={100}
+            x={GRID_CELL_SIZE}
+            y={GRID_CELL_SIZE}
+            width={GRID_CELL_SIZE}
+            height={GRID_CELL_SIZE}
             fill="red"
-            shadowBlur={10}
           />
         </Layer>
       </Stage>
