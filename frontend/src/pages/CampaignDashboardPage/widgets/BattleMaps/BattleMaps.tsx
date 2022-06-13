6e1cadc0f66f5f5b4f68adcd9b19c6a80eb2ef78
Frank Matranga <thefrankmatranga@gmail.com>
2022-06-13 12:02:36 -0400

    Add new battle maps

--- frontend/src/pages/CampaignDashboardPage/widgets/BattleMaps/BattleMaps.tsx
+++ frontend/src/pages/CampaignDashboardPage/widgets/BattleMaps/BattleMaps.tsx
@@ -1,16 +1,31 @@
-import { Box, SimpleGrid, Skeleton } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  SimpleGrid,
+  Skeleton,
+  useToast,
+  VStack,
+} from "@chakra-ui/react";
 import { ErrorAlert } from "components/ErrorAlert";
 import { BattleMap } from "ddtools-types";
 import { collection, FirestoreDataConverter, query } from "firebase/firestore";
+import { Field, Formik, FormikHelpers } from "formik";
 import { CampaignUserContext } from "pages/CampaignDashboardPage/context";
 import { useContext, useEffect, useRef, useState } from "react";
 import { useCollectionData } from "react-firebase-hooks/firestore";
+import { BattleMapAPI } from "services/api";
 import { converter, FirestoreDoc } from "services/converter";
 import { firestore } from "services/firebase";
 import { BattleMapCanvas } from "./BattleMapCanvas";
 
+type NewBattleMap = Pick<BattleMap, "name">;
+
 export function BattleMaps() {
-  const { campaign } = useContext(CampaignUserContext);
+  const toast = useToast();
+  const { campaign, user } = useContext(CampaignUserContext);
   // Firestore data
   const [battleMaps, isBattleMapsLoading, battleMapsError] = useCollectionData(
     query(
@@ -31,6 +46,34 @@ export function BattleMaps() {
       console.warn(battleMapsError);
     }
   }, [battleMapsError]);
+
+  /** Attempt to add new battle map. */
+  async function handleAddMap(
+    values: NewBattleMap,
+    formikHelpers: FormikHelpers<NewBattleMap>,
+  ) {
+    try {
+      BattleMapAPI.add(user.uid, campaign.id, values);
+      toast({
+        title: `Added New Battle Map`,
+        description: "Click on the map to edit it.",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+    } catch (error) {
+      console.error(error);
+      toast({
+        title: `An Error Occurred`,
+        description: "Failed to add new battle map. Please try again later.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    } finally {
+      formikHelpers.setSubmitting(false);
+    }
+  }
 
   return (
     <Box ref={boxRef} minW="100%" minH="100%" position="relative">
@@ -56,6 +99,41 @@ export function BattleMaps() {
               <Skeleton height={"200px"} />
             </>
           )}
+          <Box padding="6" borderWidth={1} borderRadius="lg">
+            <Formik
+              initialValues={{
+                name: "",
+              }}
+              onSubmit={handleAddMap}
+            >
+              {({ handleSubmit, isSubmitting }) => (
+                <form onSubmit={handleSubmit}>
+                  <VStack align="start">
+                    <FormControl>
+                      <FormLabel htmlFor="map-name">
+                        New Battle Map Name
+                      </FormLabel>
+                      <Field
+                        as={Input}
+                        name="name"
+                        id="map-name"
+                        placeholder="Cool Map"
+                        required
+                      />
+                    </FormControl>
+                    <Button
+                      type="submit"
+                      colorScheme="teal"
+                      isLoading={isSubmitting}
+                      loadingText="Adding map"
+                    >
+                      Add
+                    </Button>
+                  </VStack>
+                </form>
+              )}
+            </Formik>
+          </Box>
           {battleMaps?.map((map) => (
             <Box
               key={map.id}
