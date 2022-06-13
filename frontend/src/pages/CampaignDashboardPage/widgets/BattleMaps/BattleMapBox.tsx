e012fe14121287dfa137dd54c363f9c6e6b879a2
Frank Matranga <thefrankmatranga@gmail.com>
2022-06-13 13:47:49 -0400

    Make battle map box background thumbnail

--- frontend/src/pages/CampaignDashboardPage/widgets/BattleMaps/BattleMapBox.tsx
+++ frontend/src/pages/CampaignDashboardPage/widgets/BattleMaps/BattleMapBox.tsx
@@ -1,9 +1,10 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading } from "@chakra-ui/react";
 import { BattleMap } from "ddtools-types";
 import { ref } from "firebase/storage";
 import { CampaignUserContext } from "pages/CampaignDashboardPage/context";
 import { useContext } from "react";
 import { useDownloadURL } from "react-firebase-hooks/storage";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { FirestoreDoc } from "services/converter";
 import { storage } from "services/firebase";
 
@@ -12,7 +13,7 @@ type BattleMapBoxPropTypes = {
   onSelect: () => void;
 };
 export function BattleMapBox({ map, onSelect }: BattleMapBoxPropTypes) {
-  const { campaign, user } = useContext(CampaignUserContext);
+  const { userRole } = useContext(CampaignUserContext);
 
   const [downloadURL] = useDownloadURL(
     map.thumbnailFilePath && map.thumbnailFilePath.length
@@ -27,9 +28,26 @@ export function BattleMapBox({ map, onSelect }: BattleMapBoxPropTypes) {
       borderWidth={1}
       borderRadius="lg"
       onClick={onSelect}
+      bgImage={downloadURL}
+      backgroundSize="cover"
+      backgroundPosition="center"
+      minH={"250px"}
     >
-      {map.name}
-      <Image src={downloadURL} alt="Map preview" />
+      <Flex justify="space-between" h="100%">
+        <Heading size="md">{map.name}</Heading>
+        {userRole === "dm" && (
+          <Box>
+            <Button
+              size="sm"
+              leftIcon={map.isActive ? <FaEye /> : <FaEyeSlash />}
+              colorScheme={map.isActive ? "green" : "pink"}
+              onClick={(e) => e.stopPropagation()}
+            >
+              {map.isActive ? "Visible" : "Invisible"} to Players
+            </Button>
+          </Box>
+        )}
+      </Flex>
     </Box>
   );
 }
