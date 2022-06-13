dce03f1d3ef9d30ee74ace94747737ff8009f635
Frank Matranga <thefrankmatranga@gmail.com>
2022-06-13 12:02:27 -0400

    Extract update message util function

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,2 +1,10 @@
 export const clamp = (value: number, min: number, max: number): number =>
   Math.min(Math.max(value, min), max);
+
+/** Given an object of updates like {a:2, b: 'hello'}, generates a string message:
+ * "Field `a` set to `'2'`, Field `b` set to `'hello'`"
+ */
+export const updateMessage = (updates: Record<string, any>) =>
+  Object.entries(updates)
+    .map(([field, newValue]) => `Field \`${field}\` set to \`'${newValue}'\``)
+    .join(", ");
