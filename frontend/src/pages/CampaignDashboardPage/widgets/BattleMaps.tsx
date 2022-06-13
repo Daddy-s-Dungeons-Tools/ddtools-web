45670ddba3dc06b8f300fa689a1444cc81550352
Frank Matranga <thefrankmatranga@gmail.com>
2022-06-12 22:05:47 -0400

    Limit all dimensions of panning battle map stage

--- frontend/src/pages/CampaignDashboardPage/widgets/BattleMaps.tsx
+++ frontend/src/pages/CampaignDashboardPage/widgets/BattleMaps.tsx
@@ -4,6 +4,7 @@ import { Shape, ShapeConfig } from "konva/lib/Shape";
 import { Stage as KonvaStage } from "konva/lib/Stage";
 import { useEffect, useMemo, useRef, useState } from "react";
 import { Layer, Line, Rect, Stage } from "react-konva";
+import { clamp } from "utils";
 import { debounce } from "utils/debounce";
 
 /** Size of grid cell in pixels */
@@ -13,6 +14,7 @@ const GRID_HEIGHT = 1000;
 const SCALE_BY = 1.2;
 const SCALE_MAX = 3;
 const SCALE_MIN = 0.5;
+const STAGE_DRAG_PADDING = 500;
 
 export function BattleMaps() {
   const boxRef = useRef<HTMLDivElement>(null);
@@ -58,6 +60,82 @@ export function BattleMaps() {
     return () => window.removeEventListener("resize", debouncedResizer);
   }, []);
 
+  /** Given a scroll event, apply scaling to the stage. */
+  function handleScrollWheel(konvaEvent: KonvaEventObject<WheelEvent>) {
+    konvaEvent.evt.preventDefault();
+
+    const curStage = konvaEvent.target.getStage()!;
+    const oldScale = curStage.scaleX();
+    const mousePointTo = {
+      x: curStage.getPointerPosition()!.x / oldScale - curStage.x() / oldScale,
+      y: curStage.getPointerPosition()!.y / oldScale - curStage.y() / oldScale,
+    };
+
+    let newScale =
+      konvaEvent.evt.deltaY < 0 ? oldScale * SCALE_BY : oldScale / SCALE_BY;
+
+    newScale = Math.min(Math.max(newScale, SCALE_MIN), SCALE_MAX);
+    let newX =
+      (curStage.getPointerPosition()!.x / newScale - mousePointTo.x) * newScale;
+    let newY =
+      (curStage.getPointerPosition()!.y / newScale - mousePointTo.y) * newScale;
+
+    const stageBoundingBox = getStageBoundingBox();
+
+    if (newScale !== oldScale) {
+      setStage({
+        ...stage,
+        scale: clamp(newScale, SCALE_MIN, SCALE_MAX),
+        x: clamp(newX, stageBoundingBox[2], stageBoundingBox[0]),
+        y: clamp(newY, stageBoundingBox[3], stageBoundingBox[1]),
+      });
+    }
+  }
+
+  /** Update the React state for the stage's position after ending a drag. */
+  function handleStageDragEnd(konvaEvent: KonvaEventObject<DragEvent>) {
+    setStage({
+      ...stage,
+      x: konvaEvent.target.x(),
+      y: konvaEvent.target.y(),
+    });
+  }
+
+  /** Limit how far the stage can be dragged. */
+  function handleStageDragMove(konvaEvent: KonvaEventObject<DragEvent>) {
+    const boundingBox = getStageBoundingBox();
+    if (konvaEvent.target.x() > boundingBox[0]) {
+      konvaEvent.target.x(boundingBox[0]);
+    }
+
+    if (konvaEvent.target.y() > boundingBox[1]) {
+      konvaEvent.target.y(boundingBox[1]);
+    }
+
+    // TODO: add limits for other directions based on image size
+    if (konvaEvent.target.x() < boundingBox[2]) {
+      konvaEvent.target.x(boundingBox[2]);
+    }
+
+    if (konvaEvent.target.y() < boundingBox[3]) {
+      konvaEvent.target.y(boundingBox[3]);
+    }
+  }
+
+  /** Snape the target shape to the nearest grid cell. */
+  function snapToGrid(target: Shape<ShapeConfig> | KonvaStage) {
+    target.to({
+      x: Math.round(target.x() / GRID_CELL_SIZE) * GRID_CELL_SIZE,
+      y: Math.round(target.y() / GRID_CELL_SIZE) * GRID_CELL_SIZE,
+      duration: 0.1,
+    });
+  }
+
+  /** x1,y1, x2,y2 */
+  function getStageBoundingBox(): [number, number, number, number] {
+    return [STAGE_DRAG_PADDING, STAGE_DRAG_PADDING, -500, -400];
+  }
+
   const gridLines = useMemo(() => {
     return [...Array(GRID_HEIGHT / GRID_CELL_SIZE + 1)]
       .map((_, index) => (
@@ -88,52 +166,6 @@ export function BattleMaps() {
       );
   }, []);
 
-  function handleScrollWheel(konvaEvent: KonvaEventObject<WheelEvent>) {
-    konvaEvent.evt.preventDefault();
-
-    const curStage = konvaEvent.target.getStage()!;
-    const oldScale = curStage.scaleX();
-    const mousePointTo = {
-      x: curStage.getPointerPosition()!.x / oldScale - curStage.x() / oldScale,
-      y: curStage.getPointerPosition()!.y / oldScale - curStage.y() / oldScale,
-    };
-
-    let newScale =
-      konvaEvent.evt.deltaY < 0 ? oldScale * SCALE_BY : oldScale / SCALE_BY;
-
-    newScale = Math.min(Math.max(newScale, SCALE_MIN), SCALE_MAX);
-
-    if (newScale !== oldScale) {
-      setStage({
-        ...stage,
-        scale: Math.min(Math.max(newScale, SCALE_MIN), SCALE_MAX),
-        x:
-          (curStage.getPointerPosition()!.x / newScale - mousePointTo.x) *
-          newScale,
-        y:
-          (curStage.getPointerPosition()!.y / newScale - mousePointTo.y) *
-          newScale,
-      });
-    }
-  }
-
-  function handleStageDragEnd(konvaEvent: KonvaEventObject<DragEvent>) {
-    setStage({
-      ...stage,
-      x: konvaEvent.target.x(),
-      y: konvaEvent.target.y(),
-    });
-  }
-
-  /** Snape the target shape to the nearest grid cell. */
-  function snapToGrid(target: Shape<ShapeConfig> | KonvaStage) {
-    target.to({
-      x: Math.round(target.x() / GRID_CELL_SIZE) * GRID_CELL_SIZE,
-      y: Math.round(target.y() / GRID_CELL_SIZE) * GRID_CELL_SIZE,
-      duration: 0.1,
-    });
-  }
-
   return (
     <Box ref={boxRef} minW="100%" minH="100%">
       <Stage
@@ -145,6 +177,7 @@ export function BattleMaps() {
         scaleY={stage.scale}
         onWheel={handleScrollWheel}
         draggable
+        onDragMove={handleStageDragMove}
         onDragEnd={handleStageDragEnd}
       >
         <Layer id="grid" draggable={false} x={0} y={0}>
